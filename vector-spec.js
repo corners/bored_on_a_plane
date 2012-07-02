f0bb3332078a799e2cf56096f086340c9d7a8019
Dan Ward <dan.j.ward@gmail.com>
2012-07-02 13:30:15 +0100

    started switch to vectors

--- vector-spec.js
+++ vector-spec.js
@@ -55,11 +55,58 @@ describe("vector", function () {
 	});
 
 	it("can calculate the unit normal", function () {
-		var l = new Vector(5, 0),
+		var l = new Vector(4, -2),
 			a = l.getUnitNormal();
 
 		expect(a).toEqual(new Vector(0, 1));
 	});
+
+	it("can calculate the projection", function () {
+		var I = new Vector(-4, 2),
+			unitNormal = I.getUnitNormal(),
+			N = Vector.project(I, unitNormal);
+
+		expect(N).toEqual(new Vector(0, -2));
+	});
+
+	it("can calculate the reflection", function () {
+		var I = new Vector(4, -2),
+		    unitNormal = I.getUnitNormal(),
+			R = Vector.reflect(I, unitNormal);
+
+		expect(R).toEqual(new Vector(4, 2));
+	});
+
+	// todo - not part of vector
+	it("can determine if two line segments intersect", function () {
+		var p0 = new Vector(4, 7),
+			p1 = new Vector(16, 3),
+			p2 = new Vector(1, 1),
+			p3 = new Vector(17, 10),
+			a = Vector.segmentsIntersect(p0, p1, p2, p3);
+
+		expect(a).toEqual(true);
+	});
+
+	it("can determine that two line segments do not intersect", function () {
+		var p0 = new Vector(4, 7),
+			p1 = new Vector(16, 3),
+			p2 = new Vector(10, 6),
+			p3 = new Vector(17, 10),
+			a = Vector.segmentsIntersect(p0, p1, p2, p3);
+
+		expect(a).toEqual(false);
+	});
+
+
+
+
+
+
+
+
+
+
 /*
 	it("can calculate the unit normal for a real example", function() {
 		var p1 = new Vector(0, 0),
--- vector.js
+++ vector.js
@@ -1,5 +1,4 @@
-
-  "use strict"
+  "use strict";
 
   function Vector(x, y) {
     this.x = x;
@@ -32,10 +31,9 @@
     // divide each component by the length of the line
     var n = this.getPerpendicular();
     var len = Math.sqrt(Math.pow(n.x, 2) + Math.pow(n.y, 2));
-    return new Vector(n.x / len, n.y / len);
+    return new Vector(Math.round(n.x / len), Math.round(n.y / len));
     // todo proper vector multiply
   };
-
 
   Vector.prototype.multiplyScalar = function (n) {
     return new Vector(this.x * n, this.y * n);
@@ -49,21 +47,60 @@
     return new Vector(this.x + v.x, this.y + v.y);
   };
 
-    function Line(x1, y1, x2, y2) {
-    this.pt1 = new Vector(x1,y1);
-    this.pt2 = new Vector(x2,y2);
+  Vector.prototype.subtract = function (v) {
+    return new Vector(this.x - v.x, this.y - v.y);
+  };
+
+  Vector.project = function (I, unitNormal) {
+    return unitNormal.multiplyScalar(Vector.dotProduct(I.negative(), unitNormal));
+  };
+
+  // Reflects initial vector I along the unit normal
+  // I = initial vector
+  // unitNormal = unitNormal of line to reflect against
+  Vector.reflect = function (I, unitNormal) {
+  	var N = Vector.project(I, unitNormal);
+
+  	return N.multiplyScalar(2).add(I);
+  };
+
+  Vector.segmentsIntersect = function (p0, p1, p2, p3) {
+      var s1 = p1.subtract(p0),
+        s2 = p3.subtract(p2),
+        a = (-s1.y * (p0.x - p2.x)) + (s1.x * (p0.y - p2.y)),
+        b = (-s2.x * s1.y) + (s1.x * s2.y),
+        s =  a / b,
+        t = (s2.x * (p0.y - p2.y) - s2.y * (p0.x - p2.x)) / (-s2.x * s1.y + s1.x * s2.y);
+
+      return s >= 0 && s <= 1 && t >= 0 && t <= 1;
+  };
+
+
+  function Line(x1, y1, x2, y2) {
+    this.pt1 = new Vector(x1, y1);
+    this.pt2 = new Vector(x2, y2);
+	this.strokeStyle = 'red';
     // vector from pt1 to pt2
-    var v = this.pt1.toPoint(this.pt2);
+/*    var v = this.pt1.toPoint(this.pt2);
     this.v = v;
     this.P = v.getPerpendicular();
     // the unit version of perpendicular
     this.n1 = v.getUnitNormal();
+*/
   }
+
+  Line.prototype.vector = function () {
+    return this.pt1.toPoint(this.pt2);
+  };
+
+  Line.intersects = function (l1, l2) {
+	return Vector.segmentsIntersect(l1.pt1, l1.pt2, l2.pt1, l2.pt2);
+  };
 
   Line.prototype.toString = function () {
     return 'line from ' + this.pt1 + ' to ' + this.pt2 + ' unit version of perpendicuar=' + this.n1;
   };
-
+/*
   Line.prototype.collide = function (I) {
     // I is the initial vector I
     var n = Vector.dotProduct(I.negative(), this.n1);
@@ -79,9 +116,4 @@
   var result = line.collide(new Vector(4,-2));
 //  test('collide', new Vector(4,2), result);
 
-
-
-
-
-
-
+*/
