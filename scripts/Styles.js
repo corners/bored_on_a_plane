cbb6be01dfaa5eec262a009273589fc9f49d7e66
Dan Ward <dan.j.ward@gmail.com>
2013-12-31 17:05:32 +0000

    centralised styles

---
+++ scripts/Styles.js
@@ -0,0 +1,25 @@
+define(
+	// module name
+	"Styles",
+
+	[],
+
+	//The function to execute when all dependencies have loaded. The arguments
+	//to this function are the array of dependencies mentioned above.
+	function () {    
+		"use strict";
+
+		function Style(fill, stroke) {
+			this.Fill = fill;
+			this.Stroke = stroke;
+		}
+
+		return {
+			Game: { Fill: '#00a', Stroke: 'white', Font: '10pt sans-serif' },
+			Paddle: new Style('silver', 'black'),
+			BlockStyle: {
+				0: new Style('red', 'black')
+			}
+		};
+	}
+);
